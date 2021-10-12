5f4de76f85e45b24fc8302de2f4903d1eb71e17d
Doreen Fan <dfan@lbl.gov>
2021-10-12 13:46:18 -0700

    Add model with dropout layers

--- maestroflame/maestroflame/networks.py
+++ maestroflame/maestroflame/networks.py
@@ -46,6 +46,33 @@ class Net_tanh(nn.Module):
         return x
 
 
+# Net with dropout
+class Net_drop(nn.Module):
+    def __init__(self, input_size, h1, h2, h3, output_size):
+        super().__init__()
+        self.fc1 = nn.Linear(input_size, h1)
+        self.ac1 = nn.Tanh()
+        self.fc2 = nn.Linear(h1, h2)
+        self.ac2 = nn.Tanh()
+        self.fc3 = nn.Linear(h2, h3)
+        self.ac3 = nn.Tanh()
+        self.fc4 = nn.Linear(h3, output_size)
+        self.dropout = nn.Dropout(0.2)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.ac1(x)
+        x = self.dropout(x)
+        x = self.fc2(x)
+        x = self.ac2(x)
+        x = self.dropout(x)
+        x = self.fc3(x)
+        x = self.ac3(x)
+        x = self.dropout(x)
+        x = self.fc4(x)
+        return x
+
+
 class Deep_Net(nn.Module):
     def __init__(self, input_size, h1, h2, h3, h4, h5, h6, h7, output_size):
         super().__init__()
